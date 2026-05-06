a789297b7807f5c7d99cac749f30fb9db59acf60
oyghen <oyghen@duck.com>
2026-05-06 21:20:13 +0200

    refactor: move imports down

--- src/clsforge/__init__.py
+++ src/clsforge/__init__.py
@@ -2,8 +2,9 @@ __all__ = ("__version__", "FrozenClassError", "FrozenClassMeta", "EnumMixin")
 
 from importlib import metadata
 
+__version__ = metadata.version(__name__)
+
+
 from clsforge.exceptions import FrozenClassError
 from clsforge.meta import FrozenClassMeta
 from clsforge.mixin import EnumMixin
-
-__version__ = metadata.version(__name__)
