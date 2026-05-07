fa89ad3fd54b4ea2689dc7f4ccda7e8b2e13dfb6
oyghen <oyghen@duck.com>
2026-05-07 16:48:31 +0200

    style: remove blank line

--- src/clsforge/__init__.py
+++ src/clsforge/__init__.py
@@ -11,7 +11,6 @@ from importlib import metadata
 
 __version__ = metadata.version(__name__)
 
-
 from clsforge.exceptions import ClsForgeError, FrozenClassError, InvalidChoiceError
 from clsforge.meta import FrozenClassMeta
 from clsforge.mixin import EnumMixin
